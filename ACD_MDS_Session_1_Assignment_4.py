3a557a6ea686d81aa31c0ef4cdc83856a00934b8
Manish Shukla <shukla.me@gmail.com>
2018-01-26 22:33:30 +0530

    comit

---
+++ ACD_MDS_Session_1_Assignment_4.py
@@ -0,0 +1,26 @@
+
+# coding: utf-8
+
+# In[29]:
+
+
+import math
+dia= 12
+volume= (4/3)*math.pi*dia**3
+print(volume)
+
+
+# In[15]:
+
+
+list1 = ['1', '2', '3']
+str1 = ''.join(list1)
+print(str1)
+
+
+# In[10]:
+
+
+mylist = ['spam', 'ham', 'eggs']
+print ', '.join(mylist)
+
